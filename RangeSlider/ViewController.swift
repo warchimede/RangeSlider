7219132ac553647a16141ac25be7d10c441c6e5c
William Archimede <william.archimede@hoodbrains.com>
2014-09-08 18:38:56 +0200

    Initial Commit

---
+++ RangeSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RangeSlider
+//
+//  Created by William Archimede on 08/09/2014.
+//  Copyright (c) 2014 HoodBrains. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
