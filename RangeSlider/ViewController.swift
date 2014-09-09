f6ca6c2fa435b1b56a640f29fc344f2f1fd73915
William Archimede <william.archimede@hoodbrains.com>
2014-09-09 16:42:57 +0200

    Added a slider for example

--- RangeSlider/ViewController.swift
+++ RangeSlider/ViewController.swift
@@ -9,21 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let rangeSlider = RangeSlider(frame: CGRectZero)
+    let rangeSlider1 = RangeSlider(frame: CGRectZero)
+    let rangeSlider2 = RangeSlider(frame: CGRectZero)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        view.addSubview(rangeSlider)
+        rangeSlider2.trackHighlightTintColor = UIColor.redColor()
+        rangeSlider2.curvaceousness = 0.0
         
-        rangeSlider.addTarget(self, action: "rangeSliderValueChanged:", forControlEvents: .ValueChanged)
+        view.addSubview(rangeSlider1)
+        view.addSubview(rangeSlider2)
+        
+        rangeSlider1.addTarget(self, action: "rangeSliderValueChanged:", forControlEvents: .ValueChanged)
     }
     
     override func viewDidLayoutSubviews() {
         let margin: CGFloat = 20.0
         let width = view.bounds.width - 2.0 * margin
-        rangeSlider.frame = CGRect(x: margin, y: margin + topLayoutGuide.length,
+        rangeSlider1.frame = CGRect(x: margin, y: margin + topLayoutGuide.length,
             width: width, height: 31.0)
+        rangeSlider2.frame = CGRect(x: margin + 20, y: 5 * margin + topLayoutGuide.length,
+            width: width - 40, height: 40)
     }
     
     override func didReceiveMemoryWarning() {
