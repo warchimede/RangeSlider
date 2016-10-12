3cb0ec78182153a1e08029594f91c823df20f3cf
William Archimède <william.archimede@hoodbrains.com>
2016-10-12 18:39:15 +0200

    Created framework target and added support for Carthage

---
+++ RangeSliderFramework/RangeSliderFramework.h
@@ -0,0 +1,19 @@
+//
+//  RangeSliderFramework.h
+//  RangeSliderFramework
+//
+//  Created by William Archimède on 12/10/2016.
+//  Copyright © 2016 William Archimède. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RangeSliderFramework.
+FOUNDATION_EXPORT double RangeSliderFrameworkVersionNumber;
+
+//! Project version string for RangeSliderFramework.
+FOUNDATION_EXPORT const unsigned char RangeSliderFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RangeSliderFramework/PublicHeader.h>
+
+
