6b599ac8ff38005e6bde2b99c4bc573fd6e560e5
NSCabezon <nscabezon@gmail.com>
2017-11-28 12:50:44 +0100

    editing ended

--- RangeSlider/RangeSlider.swift
+++ RangeSlider/RangeSlider.swift
@@ -289,5 +289,7 @@ public class RangeSlider: UIControl {
     override public func endTracking(_ touch: UITouch?, with event: UIEvent?) {
         lowerThumbLayer.highlighted = false
         upperThumbLayer.highlighted = false
+		
+		sendActions(for: .editingDidEnd)
     }
 }
