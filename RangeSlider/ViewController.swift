48eb521781c71a29f358822e7bf73ce094e7ad38
Jason Buckner <jason@jasonbuckner.com>
2019-03-25 21:08:52 -0700

    Update project to Swift 5
    Xcode 10.2 doesn't support Swift 3 so RangeSlider wouldn't compile.

--- RangeSlider/ViewController.swift
+++ RangeSlider/ViewController.swift
@@ -38,7 +38,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func rangeSliderValueChanged(_ rangeSlider: RangeSlider) {
+    @objc func rangeSliderValueChanged(_ rangeSlider: RangeSlider) {
         print("Range slider value changed: (\(rangeSlider.lowerValue) , \(rangeSlider.upperValue))")
     }
 }
