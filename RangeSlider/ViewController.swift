fe784cef02e31b274a9f5e9bc04ea087cfcc555d
Varun Naharia <v.naharia@logictrixtech.com>
2020-11-25 17:41:01 +0530

    added sticky functionality

--- RangeSlider/ViewController.swift
+++ RangeSlider/ViewController.swift
@@ -11,17 +11,20 @@ import UIKit
 class ViewController: UIViewController {
     let rangeSlider1 = RangeSlider(frame: CGRect.zero)
     let rangeSlider2 = RangeSlider(frame: CGRect.zero)
+    let rangeSlider3 = RangeSlider(frame: CGRect.zero)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        rangeSlider2.trackHighlightTintColor = UIColor.red
-        rangeSlider2.curvaceousness = 0.0
-        
+        rangeSlider3.trackHighlightTintColor = UIColor.red
+        rangeSlider3.curvaceousness = 0.0
+        rangeSlider2.sticky = true
         view.addSubview(rangeSlider1)
         view.addSubview(rangeSlider2)
+        view.addSubview(rangeSlider3)
         
         rangeSlider1.addTarget(self, action: #selector(ViewController.rangeSliderValueChanged(_:)), for: .valueChanged)
+        rangeSlider2.addTarget(self, action: #selector(ViewController.rangeSliderValueChanged(_:)), for: .valueChanged)
     }
     
     override func viewDidLayoutSubviews() {
@@ -29,8 +32,10 @@ class ViewController: UIViewController {
         let width = view.bounds.width - 2.0 * margin
         rangeSlider1.frame = CGRect(x: margin, y: margin + topLayoutGuide.length + 100,
             width: width, height: 31.0)
-        rangeSlider2.frame = CGRect(x: margin + 20, y: 5 * margin + topLayoutGuide.length + 100,
-            width: width - 40, height: 40)
+        rangeSlider2.frame = CGRect(x: margin, y: 5 * margin + topLayoutGuide.length + 100,
+            width: width, height: 31.0)
+        rangeSlider3.frame = CGRect(x: margin + 20, y: 10 * margin + topLayoutGuide.length + 100,
+                                    width: width - 40, height: 40)
     }
     
     override func didReceiveMemoryWarning() {
