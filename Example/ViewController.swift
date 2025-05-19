4a3f970dc32ded2a676ea8794de478b384747648
William Archimede <1151996+warchimede@users.noreply.github.com>
2025-05-19 18:09:38 +0200

    chore(SPM): Add support for SPM along with the introduction of the new Example project

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -1,45 +1,31 @@
-//
-//  ViewController.swift
-//  RangeSlider
-//
-//  Created by William Archimede on 08/09/2014.
-//  Copyright (c) 2014 HoodBrains. All rights reserved.
-//
-
+import WARangeSlider
 import UIKit
 
 class ViewController: UIViewController {
     let rangeSlider1 = RangeSlider(frame: CGRect.zero)
     let rangeSlider2 = RangeSlider(frame: CGRect.zero)
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         rangeSlider2.trackHighlightTintColor = UIColor.red
         rangeSlider2.curvaceousness = 0.0
-        
+
         view.addSubview(rangeSlider1)
         view.addSubview(rangeSlider2)
-        
+
         rangeSlider1.addTarget(self, action: #selector(ViewController.rangeSliderValueChanged(_:)), for: .valueChanged)
     }
-    
+
     override func viewDidLayoutSubviews() {
         let margin: CGFloat = 20.0
         let width = view.bounds.width - 2.0 * margin
-        rangeSlider1.frame = CGRect(x: margin, y: margin + topLayoutGuide.length + 100,
-            width: width, height: 31.0)
-        rangeSlider2.frame = CGRect(x: margin + 20, y: 5 * margin + topLayoutGuide.length + 100,
+        rangeSlider1.frame = CGRect(x: margin, y: margin + 100, width: width, height: 31.0)
+        rangeSlider2.frame = CGRect(x: margin + 20, y: 5 * margin + 100,
             width: width - 40, height: 40)
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+
     @objc func rangeSliderValueChanged(_ rangeSlider: RangeSlider) {
         print("Range slider value changed: (\(rangeSlider.lowerValue) , \(rangeSlider.upperValue))")
     }
 }
-
